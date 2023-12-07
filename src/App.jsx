acdaba9eae9a41e6b38eddbbd7bf3c59b23dd582
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-07 11:59:21 +0330

    چرا باید کامپوننت ها را به کامپوننت های کوچک تر تقسیم کنیم؟

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
+const title = "React";
+const list = [
+  {
+    id: 0,
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Reza ahmadi",
+    num_comments: 3,
+    points: 4,
+  },
+  {
+    id: 1,
+    title: "React",
+    url: "https://redux.js.org",
+    author: "Mohammda ",
+    num_comments: 2,
+    points: 5,
+  },
+];
 function App() {
-  const title = "React";
-  const list = [
-    {
-      id: 0,
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Reza ahmadi",
-      num_comments: 3,
-      points: 4,
-    },
-    {
-      id: 1,
-      title: "React",
-      url: "https://redux.js.org",
-      author: "Mohammda ",
-      num_comments: 2,
-      points: 5,
-    },
-  ];
   return (
     <div>
       <h1>Hello React{title}</h1>
+      <Search/>
+      <List />
+    </div>
+  );
+}
+function List() {
+  return (
+    <ul>
+      {list.map(function (item, index) {
+        return (
+          <li key={index}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search</label>
       <input type="text" id="search" />
     </div>
   );
 }
-
 export default App;
