8b4082853d794ceff9387c8939dd3777eabf51f3
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-07 11:33:10 +0330

    نحوه استفاده از مپ

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 function App() {
   const title = "React";
+  const list = [
+    {
+      id: 0,
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Reza ahmadi",
+      num_comments: 3,
+      points: 4,
+    },
+    {
+      id: 1,
+      title: "React",
+      url: "https://redux.js.org",
+      author: "Mohammda ",
+      num_comments: 2,
+      points: 5,
+    },
+  ];
   return (
     <div>
       <h1>Hello React{title}</h1>
       <label htmlFor="search">Search</label>
-      <input type="text" id="search" />  
+      <input type="text" id="search" />
     </div>
   );
 }
